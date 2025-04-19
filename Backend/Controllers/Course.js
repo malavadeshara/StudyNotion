59b356fdc265539917427c237dabd9ce74c86760
Malav Adeshara <adesharamalav15124@gmail.com>
2025-04-19 21:29:01 +0530

    19th April Commit

---
+++ Backend/Controllers/Course.js
@@ -0,0 +1,107 @@
+const Course = require("../Models/Course");
+const Tag = require("../Models/Tags");
+const User = require("../Models/User");
+const { uploadImageToCloudinary } = require("../utils/imageUploader");
+
+// createCourse Handler Function
+exports.createCourse = async (req, res) => {
+    try {
+        // fetch data
+        const { courseName, courseDescription, whatYouWillLearn, price, tag } = req.body;
+
+        // get thumbnail
+        const thumbnail = req.files.thumbnailImage;
+
+        // validation
+        if (!courseDescription || !courseName || !whatYouWillLearn || !price || !tag || !thumbnail) {
+            return res.status(400).json({
+                success: "false",
+                message: "All Fieds are requires"
+            });
+        }
+
+        // check for instructor
+        const userId = req.user.id;
+        const instructorDetails = await User.findById(userId);
+        console.log("Instructor Details : ", instructorDetails);
+
+        if (!instructorDetails) {
+            return res.status(404).json({
+                success: false,
+                message: "Instructor Details not found",
+            });
+        }
+
+        // check given tag is valid or not
+        const tagDetails = await Tag.findById(tag);
+        if (!tagDetails) {
+            return res.status(404).json({
+                success: false,
+                message: "Tag details not found."
+            });
+        }
+
+        // upload image to cloudinary
+        const thumbnailImage = await uploadImageToCloudinary(thumbnail, process.env.FOLDER_NAME);
+
+        // Create an entery for new course
+        const newCourse = await Course.create({
+            courseName,
+            courseDescription,
+            instructor: instructorDetails._id,
+            whatYouWillLearn: whatYouWillLearn,
+            price,
+            tag: tagDetails._id,
+            thumbnail: thumbnailImage.secure_url,
+        });
+
+        // add the course to the user schema of instructor
+        await User.findByIdAndUpdate(
+            { _id: instructorDetails._id },
+            {
+                $push: {
+                    courses: newCourse._id,
+                }
+            },
+            { new: true },
+        );
+
+        // update the TAG ka schema
+        // TODO: HW
+
+        return res.status(200).json({
+            success: true,
+            message: "Course created successfully.",
+            data: newCourse,
+        })
+
+    } catch (error) {
+        return res.status(400).json({
+            success: false,
+            message: 'Error while creating the course',
+            error: error.message
+        });
+    }
+}
+
+
+//  get all courses
+exports.showAllCourses = async (req, res) => {
+    try {
+        const allCourses = await Course.find({}, {
+            courseName: true,
+            price: true,
+            thumbnail: true,
+            instructor: true,
+            ratingAndReviews: true,
+            studentsEnrolled: true,
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Cannot fetch course data",
+            error: error.message,
+        })
+    }
+}
---
+++ Backend/utils/imageUploader.js
@@ -0,0 +1,9 @@
+const cloudinary = require("cloudinary").v2;
+
+exports.uploadImageToCloudinary = async (filePath, folder, height, quality) => {
+    const options = { folder };
+    if (height) options.height = height;
+    if (quality) options.quality = quality;
+    options.resource_type = "auto";
+    return await cloudinary.uploader.upload(filePath, options);
+};
